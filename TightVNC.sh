ef61ab1ede0fadebd4372c4bd843b6c8a13e9639
honghu <honghu@huayuworld.org>
2018-08-14 02:52:25 -0400

    install nw-js 	modified:   TightVNC.sh 	modified:   utils/launch.sh

--- TightVNC.sh
+++ TightVNC.sh
@@ -2,4 +2,8 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-apt-get update && apt-get install -y lxde-core tightvncserver fonts-wqy-mircohei
+apt-get update && apt-get install -y lxde-core tightvncserver fonts-wqy-microhei
+apt-get install -y libnss3 libgconf-2-4 libnotify4 libnss3 libXss1
+
+echo "wget -O nwjs-v0.15.4.tar.gz https://dl.nwjs.io/v0.15.4/nwjs-v0.15.4-linux-x64.tar.gz"
+echo "tar -zxvf nwjs-v0.15.4.tar.gz"
