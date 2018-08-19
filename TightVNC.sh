95f7d94db736b9ef904bbb9e404c5696418ac1f9
honghu <honghu@huayuworld.org>
2018-08-18 23:12:22 -0400

    modified:   TightVNC.sh

--- TightVNC.sh
+++ TightVNC.sh
@@ -2,14 +2,14 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-# install x-windows, lxde/openbox/xfvb
+# install x-windows, lxde / openbox / xfvb(xterm)
 apt-get update && apt-get install -y openbox tightvncserver fonts-wqy-microhei
 
 ## /etc/rc.local
 # /bin/su -l cabox -c "perl /usr/bin/vncserver &" &
 # bash /home/cabox/workspace/noVNC/utils/launch.sh --vnc localhost:5901 &
 
-# install nwjs , "nw --url=https://google.com"
+# install nwjs
 apt-get install -y libnss3 libgconf-2-4 libnotify4 libnss3 libXss1 libgtk2.0-0:i386
-echo "wget https://dl.nwjs.io/v0.15.4/nwjs-v0.15.4-linux-x64.tar.gz && tar -zxvf nwjs-v0.15.4-linux-x64.tar.gz"
+echo "wget -O - https://dl.nwjs.io/v0.15.4/nwjs-v0.15.4-linux-x64.tar.gz | tar zxf -"
 echo "nw --url=https://google.com/?hl=zh_cn"
